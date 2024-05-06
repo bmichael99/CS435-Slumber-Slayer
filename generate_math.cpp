19bb38b333126c20392cf5485b76cf331c6b491b
Michael Brandmeyer <39311170+bmichael99@users.noreply.github.com>
2024-05-06 10:45:25 -0700

    Modified generate_math and added header file

--- generate_math.cpp
+++ generate_math.cpp
@@ -1,9 +1,68 @@
-#include "mbed.h"
+//Abir
 #include "generate_math.h"
-#include "NHD_0216HZ.h"
-#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+
+using namespace std;
 
 
 
+// Constructor
+generate_math::generate_math() {
+    // Initialize random seed
+    srand(time(0));
+    generateNewEquation();
+}
 
+// Generate a new equation
+void generate_math::generateNewEquation() {
+    operand1 = rand() % 10; // Numbers between 0 and 9
+    operand2 = rand() % 10;
+    int op = rand() % 3; // Choose an operation: 0, 1, or 2
+    switch (op) {
+        case 0:
+            operation = '+';
+            break;
+        case 1:
+            operation = '-';
+            break;
+        case 2:
+            operation = '*';
+            break;
+    }
+    calculateResult();
+}
 
+// Get the equation as a string
+string generate_math::getEquation() const {
+    return to_string(operand1) + " " + operation + " " + to_string(operand2) + " = ?";
+}
+
+// Check the answer
+bool generate_math::checkAnswer(int userAnswer) const {
+    return userAnswer == result;
+}
+
+// Get the correct answer (for display purposes)
+int generate_math::getCorrectAnswer() const {
+    return result;
+}
+
+// Helper function to calculate the result
+void generate_math::calculateResult() {
+    switch (operation) {
+        case '+':
+            result = operand1 + operand2;
+            break;
+        case '-':
+            result = operand1 - operand2;
+            break;
+        case '*':
+            result = operand1 * operand2;
+            break;
+        default:
+            cout << "Invalid operation" << endl;
+    }
+}
--- generate_math.h
+++ generate_math.h
@@ -1,19 +1,35 @@
-#include "mbed.h"
+#ifndef MATHEQUATION_H
+#define MATHEQUATION_H
+
 #include <string>
 
 using namespace std;
 
-#ifndef GENERATE_MATH_H
-#define GENERATE_MATH_H
-
 class generate_math {
+private:
+    int operand1;
+    int operand2;
+    char operation;
+    int result;
 
-private:          
-
+    // Helper function to calculate the result
+    void calculateResult();
 
 public:
-    
+    // Constructor
+    generate_math();
+
+    // Generate a new equation
+    void generateNewEquation();
+
+    // Get the equation as a string
+    string getEquation() const;
+
+    // Check the answer
+    bool checkAnswer(int userAnswer) const;
+
+    // Get the correct answer (for display purposes)
+    int getCorrectAnswer() const;
 };
 
-
-#endif //GENERATE_MATH_H
+#endif // MATHEQUATION_H
